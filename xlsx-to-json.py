0ca773adfaa91e4f880f7fd82fb58684318f3067
Welly S. L <liem.wellysetiawan@gmail.com>
2024-06-11 17:45:15 +0800

    add new line at the end of each generated JSON file

--- xlsx-to-json.py
+++ xlsx-to-json.py
@@ -34,6 +34,7 @@ def write_data_to_file(filename, data):
     """
     with open(filename, 'w') as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
+        f.write('\n')
 
 
 def process_sheet(sheet_name, dataframe, current_date):
